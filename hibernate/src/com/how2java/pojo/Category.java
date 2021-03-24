7712947b170df1fa8ea026d67809261ef0681f57
Shenhao <shblessings@163.com>
2021-03-24 20:32:26 +0800

    hibernate finish (#2)

--- hibernate/src/com/how2java/pojo/Category.java
+++ hibernate/src/com/how2java/pojo/Category.java
@@ -1,17 +1,44 @@
 package com.how2java.pojo;
 
-import lombok.Data;
-
 import java.util.Set;
 
-/**
- * @author shenhao
- * @date 2021/3/14
- */
-@Data
-public class Category {
-    private int id;
-    private String name;
-    private Set<Product> products;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="category_")
+public class Category {
+    int id;
+    String name;
+    Set<Product> products;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(fetch=FetchType.EAGER)
+    @JoinColumn(name="cid")
+    public Set<Product> getProducts() {
+        return products;
+    }
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
 }
--- hibernate/src/com/how2java/pojo/Product.java
+++ hibernate/src/com/how2java/pojo/Product.java
@@ -1,19 +1,70 @@
 package com.how2java.pojo;
 
-import lombok.Data;
-
 import java.util.Set;
 
-/**
- * @author shenhao
- * @date 2021/3/14
- */
-@Data
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="product_")
 public class Product {
-    private int id;
-    private String name;
-    private float price;
+    int id;
+    String name;
+    float price;
     Category category;
     Set<User> users;
-    int version;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @ManyToMany(cascade=CascadeType.ALL,fetch=FetchType.EAGER)
+    @JoinTable(
+            name="user_product",
+            joinColumns=@JoinColumn(name="pid"),
+            inverseJoinColumns=@JoinColumn(name="uid")
+    )
+    public Set<User> getUsers() {
+        return users;
+    }
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    @ManyToOne
+    @JoinColumn(name="cid")
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public float getPrice() {
+        return price;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
 }
