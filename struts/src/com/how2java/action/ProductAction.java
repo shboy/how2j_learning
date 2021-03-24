6077a0eef29b3838654ec8e00d8fdd1f6e1f41fe
shenhao <shblessings@163.com>
2021-03-24 23:43:01 +0800

    20210324

--- struts/src/com/how2java/action/ProductAction.java
+++ struts/src/com/how2java/action/ProductAction.java
@@ -2,12 +2,33 @@ package com.how2java.action;
 
 import com.how2java.bean.Product;
 
-public class ProductAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.ServletActionContext;
+
+import java.util.Map;
+
+public class ProductAction extends ActionSupport {
     private Product product;
 
     public String show() {
+        HttpServletRequest request = ServletActionContext.getRequest();
+        HttpServletResponse response = ServletActionContext.getResponse();
+
+        System.out.println("request:\t" + request);
+        System.out.println("response:\t" + response);
+
         product = new Product();
         product.setName("iphone7");
+        return "show";
+    }
+
+    public String add() {
+        Map m = ActionContext.getContext().getSession();
+        m.put("name", product.getName());
         return "show";
     }
 
--- struts/src/com/how2java/action/ShowTime.java
+++ struts/src/com/how2java/action/ShowTime.java
@@ -3,6 +3,9 @@ package com.how2java.action;
 public class ShowTime {
     private String currentTime;
 
+    // FIXME: 这个任务没有完成
+    // https://how2j.cn/k/struts/struts-jsp/55.html
+    // 步骤 9 : 访问路径 /showTime 跳转到页面 showTime.jsp，并且在showTime.jsp中显示当前时间
     public String getCurrentTime() {
         this.setCurrentTime(String.valueOf(System.currentTimeMillis()));
         return currentTime;
